3f5af9940d9f1bf5d88156d524c00b41bdea928e
Mayara Renata <mayararenataa@gmail.com>
2025-11-13 22:00:59 -0300

    insert implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,5 +1,6 @@
 package model.dao.impl;
 
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -25,7 +26,43 @@ public class SellerDaoJDBC implements SellerDao{
 
 	@Override
 	public void insert(Seller obj) {
-		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+
+			"INSERT INTO seller "
+			+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+			+ "VALUES "
+			+ "(?, ?, ?, ?, ?) ",
+			java.sql.Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			
+			int linhas = st.executeUpdate();
+			
+			if (linhas > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				
+			if (rs.next()) {
+				int id = rs.getInt(1);
+				obj.setId(id);
+			}
+			DB.closeResultSet(rs);
+			}
+			else {
+				throw new DbException("Nenhuma linha afetada");
+			}
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally{
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
